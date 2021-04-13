89263f34dfa099f51bc17ab7407014455cc0cb70
Richard Wang <rwang83@gmail.com>
2021-04-13 11:34:03 -0400

    update

--- projects/printer-v2/xy-motor.ts
+++ projects/printer-v2/xy-motor.ts
@@ -14,21 +14,15 @@ const m5_sunken = cylinder({ d: 5, h: inf, center: true })
 
 const smooth_rod = 8;
 
-const base = polyRound({
-  points: getRectPoints({ size: [base_size[0], base_size[1]] }),
-  radii: [0, 0, 10, 0],
-}).extrude({ height: thickness[2], $fn: 30 });
-
 const side =
   polyRound({
-    points: getRectPoints({ size: [base_size[1], base_size[2]] }),
+    points: getRectPoints({ size: [base_size[1] - 10, base_size[2]] }),
     radii: [0, 0, 40, 0],
   }).extrude({ height: thickness[1], $fn: 30 })
     .difference(
-      m5_sunken.translate([motor_size / 2 - 10, 10, 1]),
-      m5_sunken.translate([motor_size / 2 + 10, 10, 1]),
+      m5_sunken.translate([20, 10, 1]),
       cylinder({ d: smooth_rod, h: inf, center: true })
-        .translate([motor_size / 2, 20 + smooth_rod / 2 + 4, 0]))
+        .translate([motor_size / 2 - 10, 20 + smooth_rod / 2 + 4, 0]))
     .rotate([90, 0, 90]);
 
 const mount = {
@@ -41,25 +35,33 @@ const mount = {
 const m3_sunken = cylinder({ d: 3, h: inf, center: true })
   .union(cylinder({ d: 6, h: inf }));
 
+const base = polyRound({
+  points: getRectPoints({ size: [base_size[0], base_size[1]] }),
+  radii: [10, 10, 10, 10],
+}).extrude({ height: thickness[2], $fn: 30 })
+  .difference(
+    // center hole
+    cylinder({ d: mount.hole, h: inf, center: true, $fn: 100 })
+      .translate(mount.center),
+
+    // mounting screws
+    ...getRectPoints({ size: [mount.screw_spacing, mount.screw_spacing], center: true })
+      .map(c => m3_sunken.translate([c[0] + mount.center[0], c[1] + mount.center[1], mount.screw_depth]))
+
+  )
+  .translate([-10, -10, 0]);
+
 const rounding_box = polyRound({
   points: getRectPoints({ size: [inf, inf] }),
   radii: [3, 3, 3, 3],
 }).extrude({ height: inf, $fn: 10 });
 
-const res = rounding_box.intersection(
-  base
-    .difference(
-      // center hole
-      cylinder({ d: mount.hole, h: inf, center: true, $fn: 100 })
-        .translate(mount.center),
-
-      // mounting screws
-      ...getRectPoints({ size: [mount.screw_spacing, mount.screw_spacing], center: true })
-        .map(c => m3_sunken.translate([c[0] + mount.center[0], c[1] + mount.center[1], mount.screw_depth]))
-    ).union(
-      side,
+const res = base.union(
+  rounding_box.intersection(
+    side.union(
       side.mirror([-1, 1, 0])
-    )
+    ).translate([0, 0, thickness[2]])
+  )
 ).set({ $fn: 60 });
 
 export const main = res;
