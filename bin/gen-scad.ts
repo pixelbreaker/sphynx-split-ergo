ce64ad17ce69def72d3b396b959b3f6591a705e6
Richard Wang <rwang83@gmail.com>
2021-04-14 01:56:54 -0400

    add pipeline

---
+++ bin/gen-scad.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env ts-node
+import * as path from 'path';
+import * as fs from 'fs';
+
+const cwd = process.cwd();
+const watchDir = path.join(cwd, 'projects');
+const outputDir = path.join(cwd, 'target');
+
+const header = `include <${path.join(__dirname, '..', 'scad_modules', 'polyround.scad')}>`;
+const arg = process.argv[2];
+const file = arg.startsWith('.') ? path.join(process.cwd(), arg) : arg;
+
+const name = path.basename(file, '.ts');
+const target = path.join(outputDir, file.substr(watchDir.length));
+const targetDir = path.dirname(target);
+fs.mkdirSync(targetDir, { recursive: true });
+import(file).then(mod => {
+  if ('main' in mod) {
+    const src: string = header + '\n' + mod.main.src.join('\n');
+    const fileName = path.join(targetDir, name + ".scad");
+    fs.writeFile(fileName, src, e => {
+      if (e) {
+        console.log("error", fileName, e);
+      } else {
+        //  console.log(src);
+        console.log("saved", fileName);
+      }
+    });
+  }
+}).catch(e => {
+  console.log(e);
+});
---
+++ bin/pipeline.ts
@@ -0,0 +1,49 @@
+#!/usr/bin/env ts-node
+import * as path from 'path';
+import * as os from 'os';
+import { execSync } from 'child_process';
+
+const arg = process.argv[2];
+
+// get scad
+const gen_scad = path.join(__dirname, 'gen-scad.ts');
+const scad_result = execSync([
+  process.platform.startsWith('win') ? 'npm.cmd' : 'npm',
+  'run', 'ts', gen_scad, arg
+].join(' ')).toString();
+const base_file = scad_result.match(/saved (.*)\.scad/)[1];
+const scad_file = base_file + '.scad';
+console.log('scad file', scad_file);
+
+// get stl
+const stl_file = base_file + '.stl';
+const stl_result = execSync([
+  'openscad', '-o', stl_file, scad_file
+].join(' ')).toString();
+console.log(stl_result);
+console.log('stl file', stl_file);
+
+// get gcode
+const gcode_file = base_file + '.gcode';
+const gcode_result = execSync([
+  '%userprofile%\\Slic3r-1.3.0.64bit\\Slic3r-console.exe',
+  '--output', gcode_file,
+  '--load', path.join(__dirname, '..', 'slic3r_config', 'cube_normal_duramic_black.ini'),
+  stl_file
+].join(' ')).toString();
+console.log(gcode_result);
+console.log('gcode file', gcode_file);
+
+// upload
+const upload_result = execSync([
+  'curl', '-k', '-H',
+  '"X-Api-Key: E01B67DAC70E45FB87F33CE3CC0CF803"',
+  '-F', '"select=false"',
+  '-F', '"print=false"',
+  '-F', `"file=@${gcode_file}"`,
+  'http://octopi/api/files/local'
+].join(' ')).toString();
+console.log(upload_result);
+
+
+
