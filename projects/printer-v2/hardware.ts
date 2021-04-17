c4dca067e34d24d1c8e7dcdb85a3e3dc84160db7
Richard Wang <rwang83@gmail.com>
2021-04-17 03:10:02 -0400

    update

--- projects/printer-v2/hardware.ts
+++ projects/printer-v2/hardware.ts
@@ -14,15 +14,15 @@ export const pulley = {
 export const steel_rod = 8;
 export const carbon_fiber_rod = 12;
 export const lmu88 = {
-  od: 15,
-  length: 24
+  od: 16,
+  length: 18
 }
 
-export const m5 = cylinder({ d: 5, h: inf, center: true });
-export const m5_sunken = m5.union(cylinder({ d: 10, h: inf }));
+export const m5 = cylinder({ d: 5, h: 20, center: true });
+export const m5_sunken = m5.union(cylinder({ d: 10, h: 20 }));
 export const m5_countersunk = m5.union(circle({ d: 5 })
   .linear_extrude({ height: 3, scale: [2, 2] }));
-export const m3 = cylinder({ d: 3, h: inf, center: true });
-export const m3_sunken = m3.union(cylinder({ d: 6, h: inf }));
+export const m3 = cylinder({ d: 3, h: 20, center: true });
+export const m3_sunken = m3.union(cylinder({ d: 6, h: 20 }));
 
 export const clearance_pulley = cylinder({ d: pulley.brim + 2, h: 12 });
--- projects/printer-v2/y-carriage-connector.ts
+++ projects/printer-v2/y-carriage-connector.ts
@@ -7,14 +7,15 @@ import { lmu88, m5, pulley, inf, m5_sunken, m3_sunken, m3 } from "./hardware";
 
 const t = 2;
 
-const rod_od = 12.5;
+const rod_od = 12;
 const rod_offset = 17;
 const pulley_to_clasp = 25;
-const clasp_size = lmu88.od + t * 2;
-const body_size: Vec3 = [pulley_to_clasp * 2, clasp_size, lmu88.length];
+const height = lmu88.length * 2;
+const body_size: Vec3 = [pulley_to_clasp * 2, lmu88.od, height];
 const pulley_offset = pulley.brim / 2 + 0.5;
 const screw = m3.rotate([0, 90, 0]);
-const body = cube({ size: body_size, center: true })
+const body = cube({ size: body_size, center: true }).union(
+  cylinder({ d: lmu88.od + 8, h: height, center: true }))
   .difference(
     cylinder({ d: rod_od, h: inf, center: true })
       .rotate([90, 0, 0])
@@ -22,13 +23,11 @@ const body = cube({ size: body_size, center: true })
     cylinder({ d: rod_od, h: inf, center: true })
       .rotate([90, 0, 0])
       .translate([-rod_offset, 0, 0]),
-    screw.translate([body_size[0] / 2, 0, pulley_offset]),
-    screw.translate([body_size[0] / 2, 0, -pulley_offset])
-  ).difference(
+    screw.translate([body_size[0] / 2, pulley.bore, pulley_offset]),
+    screw.translate([body_size[0] / 2, pulley.bore, -pulley_offset]),
     // clasp hole
     cylinder({ d: lmu88.od, h: inf, center: true }),
-    m3.rotate([90, 0, 0]).translate([0, 0, 6]),
-    m3.rotate([90, 0, 0]).translate([0, 0, -6]),
+    m3.rotate([90, 0, 0]).translate([0, 10, height / 3]),
+    m3.rotate([90, 0, 0]).translate([0, 10, -height / 3]),
   );
-
 export const main = body.set({ $fn: 60 });
