d3604869342755ab301bba3a4aaa90aa37c4c431
Richard Wang <rwang83@gmail.com>
2021-04-19 15:13:47 -0400

    update

--- projects/printer-v2/y-carriage-connector.ts
+++ projects/printer-v2/y-carriage-connector.ts
@@ -1,45 +1,48 @@
+import { OutputSettings } from "../../bin/gen-scad";
 import { union, difference, intersection, Vec3 } from "../../src/csg/base";
-import { getCircularPoints, getDiamondPoints, getRectPoints, polyRound } from "../../src/csg/polyround";
+import { getCircularPoints, getDiamondPoints, getRectPoints, PolyRound, polyRound } from "../../src/csg/polyround";
 import { circle, square, polygon, } from "../../src/csg/primitives";
 import { cube, cylinder, sphere, ployhedron } from "../../src/csg/primitives";
 import { ring } from "../utils";
 import { lmu88, m5, pulley, inf, m5_sunken, m3_sunken, m3 } from "./hardware";
 
 const rod_od = 12;
-const pulley_to_clasp = 25;
-const height = 51;
-const body_size: Vec3 = [pulley_to_clasp, lmu88.od, height];
+const rod_offset = 20;
+const body_size: Vec3 = [55, 20, lmu88.od];
 const pulley_offset = pulley.brim / 2 + 0.5;
-const screw = m5.rotate([0, 90, 0]);
-const clasp_gap_rad = 40 * (0.5 * Math.PI / 180);
+const screw = m5.rotate([-90, 0, 0]);
+const clasp_screw_distance = 18;
+const gap_size = 1;
 
-const holes = cylinder({ d: rod_od, h: inf, center: true })
-  .union(
-    ring({ id: rod_od + 1, od: rod_od + 3, h: 2, center: true })
-      .translate([0, 0, -3]))
-  .rotate([90, 0, 0])
-  .translate([17, 0, (height - rod_od) / 2 + 0.5])
-  .union(
-    // pulley screws
-    screw.translate([body_size[0], pulley.bore, pulley_offset]),
+const holes =
+  // carbon fiber rod holes
+  cylinder({ d: rod_od, h: inf, center: true })
+    .translate([rod_offset, 0, 0])
+    .union(
+      // pulley screws
+      screw.translate([pulley_offset, 3, -2]),
 
-    // clasp hole
-    cylinder({ d: lmu88.od, h: inf, center: true }),
-    polygon({
-      points: [
-        [0, 0],
-        [-inf * Math.cos(clasp_gap_rad), inf * Math.sin(clasp_gap_rad)],
-        [-inf * Math.cos(clasp_gap_rad), -inf * Math.sin(clasp_gap_rad)]]
-    }).linear_extrude({ height: inf, center: true })
-      .rotate([0, 0, 20])
-  )
+      // clasp screws
+      m3.rotate([90, 0, 0])
+        .translate([clasp_screw_distance / 2, -body_size[1] / 2, body_size[2] / 4]),
 
-const body = cube({ size: body_size, center: true })
-  .translate([body_size[0] / 2, 3, 0])
-  .union(
-    cylinder({ d: lmu88.od + 6, h: height, center: true }))
+      // gap
+      cube({ size: [rod_offset * 2, gap_size, inf], center: true })
+        .translate([0, -rod_od / 2 + gap_size / 2, 0])
+
+    )
+
+const body = polyRound({
+  points: getRectPoints({ size: [body_size[0], body_size[1]], center: true }),
+  radii: [10, 10, 10, 10],
+}).extrude({ height: body_size[2], center: true, $fn: 30 })
   .difference(
     holes,
-    holes.mirror([0, 0, 1])
+    holes.mirror([1, 0, 0])
   );
 export const main = body.set({ $fn: 60 });
+export const settings: OutputSettings = {
+  slicer: {
+    "brim_width": 3
+  }
+}
