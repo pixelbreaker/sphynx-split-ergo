4a44f2e272c9e4dd8be4cb22b68a3a7234238020
Richard Wang <rwang83@gmail.com>
2021-04-26 02:50:15 -0400

    update

--- projects/printer-v2/pulley-holder-bearing.ts
+++ projects/printer-v2/pulley-holder-bearing.ts
@@ -7,32 +7,31 @@ import { hole, ring } from "../utils";
 import { lmu88, pulley, inf } from "./hardware";
 import { base_size } from "./xy-corner";
 
-const size = 20;
-const thickness = 4;
-const cavity_size = size - thickness * 2;
+const size = [26, 20, 22];
+const offset_z = -1;
+const t = 6;
+const tz = 4 + offset_z;
 
 const body = polyRound({
   points: getRectPoints({
-    size: [size, size],
+    size: [size[2], size[1]],
     center: true
   }),
   radii: [0, 0, 10, 10]
-}).extrude({ height: size, center: true, $fn: 60 }).rotate([0, -90, 0]);
+}).extrude({ height: size[0], center: true, $fn: 60 })
+  .rotate([0, -90, 0])
+  .translate([0, 0, offset_z]);
 
-const bearing = cylinder({ d: 16.5, h: cavity_size + 3 * 2, center: true })
-  .union(cylinder({ d: 6, h: inf, center: true }))
+const bearing = cylinder({ d: 16.5, h: size[0] - 2, center: true })
+  .union(cylinder({ d: 10, h: inf, center: true }))
   .rotate([0, 90, 0]);
 
 export const main = body.difference(
-  cube({ size: [cavity_size, inf, inf], center: true })
-    .translate([0, 0, (inf - cavity_size) / 2]),
+  cube({ size: [size[0] - t * 2, inf, inf], center: true })
+    .translate([0, 0, (inf - size[2]) / 2 + tz]),
   hole({ d: 5.5, counterbore: 10, depth: 2 })
-    .translate([0, 0, -cavity_size / 2 + 0.01]),
+    .translate([0, 0, -size[2] / 2 + tz + 0.01]),
   bearing
-).set({ $fn: 60 });
+).set({ $fn: 100 });
 
-export const settings: OutputSettings = {
-  slicer: {
-    //"brim_width": 3
-  }
-}
+
