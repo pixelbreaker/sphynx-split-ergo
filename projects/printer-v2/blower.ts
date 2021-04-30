babd0d5b9241cc832270cdd31d5aa98624d00bc6
Richard Wang <rwang83@gmail.com>
2021-04-30 00:12:35 -0400

    update

--- projects/printer-v2/blower.ts
+++ projects/printer-v2/blower.ts
@@ -8,18 +8,20 @@ import { inf } from "./hardware";
 
 const t = 2;
 const size: Vec2 = [40 + t * 2, 40 + t * 2];
-const size_min: Vec2 = [20, 10];
-const offset = [10, (size[1] - size_min[1]) / 2];
+const size_transition = 10;
+const size_min: Vec2 = [20, 5];
+const offsetFn = (n: number) => [10, (size[1] - n) / 2];
+const offset = offsetFn(size_min[1]);
 const slant = 5
 
 const base = convexTube({
   profiles: [
     square(size),
     square(size),
-    square([size[0], size_min[1]]).translate([0, offset[1], 0]),
+    square([size[0], size_transition]).translate([0, offsetFn(size_transition)[1], 0]),
     square(size_min).translate([offset[0], offset[1] + slant, 0])
   ],
-  lengths: [30, 0, 40]
+  lengths: [30, 0, 35]
 });
 const total_height = base.lengths.reduce((a: number, v: number) => a + v, 0);
 
@@ -33,18 +35,18 @@ const slot = cylinder({ d: 5, h: inf }).translate([0, 5, 0]).hull(
   cylinder({ d: 5, h: inf }).translate([0, -10, 0])
 );
 
-export const main = body.difference(
+const whole = body.difference(
   intake.translate([0, 0, intake.h / 2]),
   ...screws,
   exit_nozzle.translate([offset[0], offset[1] + slant, total_height - t / 2]),
   drop_in.translate([0, -size[1] / 2 + drop_in[1] / 2 - 0.01, drop_in[2] / 2 + t]),
-)
-  //.intersection(cube([inf, inf, inf]).translate([0, inf / 2, 0]))
-  .render()
-  .set({ $fn: 60 });
+);
+const cut = cube([inf, inf, (base.lengths[0] - t / 2 - .5) * 2]);
+
+export const main = whole.intersection(cut).union(
+  whole.difference(cut).translate([size[0] + 5, 0, -cut[2] / 2])
+).render().set({ $fn: 60 });
 
 export const settings: OutputSettings = {
-  slicer: {
-    'cut': base.lengths[0] - t
-  }
+  slicer: {}
 }
